80c53084303d359c620b8aca522e83d210ba1bd8
premblgr <prem.balagourou@gmail.com>
2022-05-10 12:23:49 +0200

    exo2

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box1}></View>
+      <View style={styles.box2}></View>
+      <View style={styles.box3}></View>
     </View>
   );
 }
@@ -13,8 +14,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection : "row"
+    
   },
+  box1: {
+    flex: 4,
+    backgroundColor : "green",
+  },
+  box2: {
+    flex: 3,
+    backgroundColor : "red",
+  },
+  box3: {
+    flex: 1,
+    backgroundColor : "blue",
+  },
+
 });
